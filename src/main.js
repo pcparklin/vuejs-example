c76284dc0bd61ca46001d0174e28f92628050e81
Jim Yeh <lemonlatte@gmail.com>
2017-01-14 23:19:18 +0800

    Add two more pages, distribution and combination

--- src/main.js
+++ src/main.js
@@ -7,9 +7,13 @@ Vue.use(VueMaterial)
 
 var Main = require('./app/main.vue')
 var Home = require('./view/home.vue')
+var Distribution = require('./view/distribution.vue')
+var Combination = require('./view/combination.vue')
 
 var routes = [
   {path: '/', component: Home},
+  {path: '/distribution', component: Distribution},
+  {path: '/combination', component: Combination},
 ]
 
 var router = new VueRouter({routes})
---
+++ src/view/distribution.vue
@@ -0,0 +1,14 @@
+<style type="text/css"></style>
+
+<template>
+<div>
+  There will be a c3 graph for distribution.
+</div>
+</template>
+
+<script type="text/javascript">
+
+export default {
+  date () {}
+}
+</script>
