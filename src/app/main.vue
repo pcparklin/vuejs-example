91d9e192a233646175e2c73d8aceea3f2cab57f6
Jim Yeh <lemonlatte@gmail.com>
2017-01-14 23:07:08 +0800

    Add a menu bar in main app

--- src/app/main.vue
+++ src/app/main.vue
@@ -1,14 +1,24 @@
 
 <style>
-body {
-  padding-top: 40px;
+.main-content {
+  padding: 20px;
 }
 </style>
 
 <template>
-<div class="jumbotron">
-  <h1>Hello, {{ who }}.</h1>
-  <p>This is a basestack for vuejs. This project integrate vuejs with gulp, vueify and browserify.</p>
+<div>
+  <md-toolbar>
+    <md-button class="md-icon-button">
+      <md-icon>menu</md-icon>
+    </md-button>
+    <h2 class="md-title" style="flex: 1">Fund</h2>
+    <md-button>Distribution</md-button>
+    <md-button>Combination</md-button>
+  </md-toolbar>
+  <div class="main-content">
+    <h1>Hello, {{ who }}.</h1>
+    <p>This is a basestack for vuejs. This project integrate vuejs with gulp, vueify and browserify.</p>
+  </div>
 </div>
 </template>
 
