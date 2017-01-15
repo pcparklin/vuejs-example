242a233ac44be952a85e6d6f6c5119041f406462
Jim Yeh <lemonlatte@gmail.com>
2017-01-15 20:32:59 +0800

    Add some example graphs by c3.js

--- src/view/combination.vue
+++ src/view/combination.vue
@@ -2,13 +2,36 @@
 
 <template>
 <div>
-  There will be a c3 graph combination.
+  <h3>This is the combination graph of funds</h3>
+  <div id="chart"></div>
 </div>
 </template>
 
 <script type="text/javascript">
+var c3 = require("c3")
 
 export default {
+  mounted () {
+    var chart = c3.generate({
+      bindto: '#chart',
+      data: {
+        columns: [
+            ['data1', -30, 200, 200, 400, -150, 250],
+            ['data2', 130, 100, -100, 200, -150, 50],
+            ['data3', -230, 200, 200, -300, 250, 250]
+        ],
+        type: 'bar',
+        groups: [
+            ['data1', 'data2', 'data3']
+        ]
+      },
+      grid: {
+          y: {
+              lines: [{value:0}]
+          }
+      }
+    })
+  },
   date () {}
 }
 </script>
