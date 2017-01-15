231eb073bdafcd9d9b64b5120738e87be000b8e0
Jim Yeh <lemonlatte@gmail.com>
2017-01-15 20:31:26 +0800

    Add links to buttons on menu bar

--- src/app/main.vue
+++ src/app/main.vue
@@ -10,9 +10,9 @@
     <md-button class="md-icon-button">
       <md-icon>menu</md-icon>
     </md-button>
-    <h2 class="md-title" style="flex: 1">Fund</h2>
-    <md-button>Distribution</md-button>
-    <md-button>Combination</md-button>
+    <h2 class="md-title" style="flex: 1"><router-link to="/">Fund</router-link></h2>
+    <router-link to="/distribution" tag="md-button">Distribution</router-link>
+    <router-link to="/combination" tag="md-button">Combination</router-link>
   </md-toolbar>
 
   <router-view class="main-content"></router-view>
